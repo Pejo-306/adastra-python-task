7be09ae1fe09a25e1fb18d3567f91ffea8b7d60e
Petar Nikolov <penikolov23@gmail.com>
2021-04-07 15:37:52 +0300

    [Sinks] Implement console data sink

--- src/sinks/console_data_sink.py
+++ src/sinks/console_data_sink.py
@@ -4,13 +4,17 @@ from src.sinks.data_sink import DataSink
 class ConsoleDataSink(DataSink):
 
     def __init__(self, output_format: str) -> None:
-        pass
+        self.output_format = output_format
 
     def initialize(self) -> None:
         pass
 
     def dump(self, message: dict) -> bool:
-        pass
+        output = self.output_format.format(message["key"],
+                                           message["value"],
+                                           message["ts"])
+        print(output, end="")
+        return True
 
     def close(self) -> None:
         pass
