7e513a1e1c00a5ff7ed136079b09e20994f5d4a3
Petar Nikolov <penikolov23@gmail.com>
2021-04-07 16:46:55 +0300

    [Sinks] Document DataSink and ConsoleDataSink

--- src/sinks/console_data_sink.py
+++ src/sinks/console_data_sink.py
@@ -2,14 +2,46 @@ from src.sinks.data_sink import DataSink
 
 
 class ConsoleDataSink(DataSink):
+    """Data sink which dumps its messages to the console (STDOUT)
+
+    Attributes:
+        output_format(str): string format of the outputted message
+
+    Methods:
+        initialize(): do nothing (see DataSink)
+        dump(message): print a single formatted message on the console (STDOUT)
+        close(): do nothing (see DataSink)
+    """
 
     def __init__(self, output_format: str) -> None:
+        """Construct console data sink
+
+        :param output_format: string format of outputted message
+        :type output_format: str
+        """
         self.output_format = output_format
 
     def initialize(self) -> None:
+        """Do nothing (see DataSink)
+
+        Console data sinks do not require any preemptive setup.
+        """
         pass
 
     def dump(self, message: dict) -> bool:
+        """Print a single message on the console
+
+        Message bodies must be mappings which contain the following keys:
+        "key", "value" and "ts". Any other keys in the mapping are ignored.
+        The outputted message is formatted with the string format which is
+        initially supplied when the data sink is created.
+
+        :param message: body of the message
+        :type message: dict
+
+        :return: status which indicates whether the dump was successful
+        :rtype: bool
+        """
         output = self.output_format.format(message["key"],
                                            message["value"],
                                            message["ts"])
@@ -17,4 +49,8 @@ class ConsoleDataSink(DataSink):
         return True
 
     def close(self) -> None:
+        """Do nothing (see DataSink)
+
+        Console data sinks do not require any cleanup/termination.
+        """
         pass
--- src/sinks/data_sink.py
+++ src/sinks/data_sink.py
@@ -2,15 +2,32 @@ from abc import ABC, abstractmethod
 
 
 class DataSink(ABC):
+    """Interface for container where data can be arbitrarily dumped
+
+    Methods:
+        initialize(): prepare the data sink for the incoming data dumps
+        dump(message): dump a single message into the sink
+        close(): clean up the sink and terminate the connection to it
+    """
 
     @abstractmethod
     def initialize(self) -> None:
+        """Prepare the data sink for the incoming data dumps"""
         pass
 
     @abstractmethod
     def dump(self, message: dict) -> bool:
+        """Dump a single message into the sink
+
+        :param message: body of the message
+        :type message: dict
+
+        :return: status which indicates whether the dump was successful
+        :rtype: bool
+        """
         pass
 
     @abstractmethod
     def close(self) -> None:
+        """Clean up the sink and terminate the connection to it"""
         pass
