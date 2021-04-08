7cdb315597f54f1ddfb10d8cdcc1400dfd96e447
Petar Nikolov <penikolov23@gmail.com>
2021-04-08 19:25:05 +0300

    [ETL] Add interface for ETL
    Each ETL process has the following interface methods:
    - source(): create an instance of a specific data source
    - sink(): create an instance of a specific data sink
    - run(): parse data from data source to data sink

--- src/etl.py
+++ src/etl.py
@@ -1,2 +1,16 @@
+from __future__ import annotations  # obsolete if using Python 3.10+
+
+from src.sources.data_source import DataSource
+from src.sinks.data_sink import DataSink
+
+
 class ETL:
-    pass
+
+    def source(self, source_cls: DataSource, *args, **kwargs) -> ETL:
+        pass
+
+    def sink(self, sink_cls: DataSink, *args, **kwargs) -> ETL:
+        pass
+
+    def run(self) -> None:
+        pass
