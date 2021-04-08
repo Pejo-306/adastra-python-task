a37f46ec89e491fedd9ec3bb600f5898fbdff0ef
Petar Nikolov <penikolov23@gmail.com>
2021-04-08 17:18:29 +0300

    [Sources] Document 'FileNotOpenError' and 'FileSourceDepleted'

--- src/exceptions/file_not_open_error.py
+++ src/exceptions/file_not_open_error.py
@@ -1,9 +1,29 @@
 class FileNotOpenError(Exception):
+    """Exception raised when attempting to read from an unopened file
+
+    Attributes:
+        filepath(str): path to unopened file
+        message(str): error message
+    """
 
     def __init__(self, filepath: str, message: str = "File is not open"):
+        """Construct FileNotOpenError
+
+        :param filepath: path to unopened file
+        :type filepath: str
+        :param message: error message
+        :type message: str
+        """
+
         self.filepath = filepath
         self.message = message
         super().__init__(message)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Get the informal string representation of the error
+
+        :return: full error message
+        :rtype: str
+        """
+
         return f"{self.filepath}: {self.message}"
--- src/exceptions/file_source_depleted.py
+++ src/exceptions/file_source_depleted.py
@@ -1,9 +1,29 @@
 class FileSourceDepleted(Exception):
+    """Exception raised when attempting to read from a depleted source file
+
+    Attributes:
+        filepath(str): path to depleted file
+        message(str): error message
+    """
 
     def __init__(self, filepath: str, message: str = "File source is depleted"):
+        """Construct FileSourceDepleted
+
+        :param filepath: path to depleted file
+        :type filepath: str
+        :param message: error message
+        :type message: str
+        """
+
         self.filepath = filepath
         self.message = message
         super().__init__(message)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Get the informal string representation of the error
+
+        :return: full error message
+        :rtype: str
+        """
+
         return f"{self.filepath}: {self.message}"
