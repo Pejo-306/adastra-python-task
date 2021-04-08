a794cdc136229b66b5aa7061967c54b430383a1f
Petar Nikolov <penikolov23@gmail.com>
2021-04-08 16:27:45 +0300

    [Sources] Implement FileSourceDepleted

--- src/exceptions/file_source_depleted.py
+++ src/exceptions/file_source_depleted.py
@@ -1,2 +1,9 @@
 class FileSourceDepleted(Exception):
-    pass
+
+    def __init__(self, filepath: str, message: str = "File source is depleted"):
+        self.filepath = filepath
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return f"{self.filepath}: {self.message}"
